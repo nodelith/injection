a46b4eca2a1723f59752572c15d445dcd465248b
Thomaz Zandonotto <thomaz.zandonotto@gmail.com>
2025-06-22 12:34:56 -0700

    Define Target type

--- src/target.ts
+++ src/target.ts
@@ -1,7 +1,7 @@
-export type Target<R, D extends readonly unknown[] = []> = (...dependencies: D) => R
+export type Target<R, D extends readonly any[] = any[]> = (...dependencies: D) => R
 
-export type TargetFactory<R extends object, P extends readonly unknown[] = []> = (...parameters: P) => R
+export type TargetFactory<R extends object, P extends readonly any[] = any[]> = (...parameters: P) => R
 
-export type TargetConstructor<R extends object, P extends readonly unknown[] = []> = 
+export type TargetConstructor<R extends object, P extends readonly any[] = any[]> = 
   | ( new (...params: P) => R ) 
   | { new (...params: P): R }
