0b1000d84c52b952e263e1f5e6540a89844e6902
Thomaz Zandonotto <thomaz.zandonotto@gmail.com>
2025-06-22 12:36:08 -0700

    Implemente Registration class

---
+++ src/registration.test.ts
@@ -0,0 +1,106 @@
+import { Registration } from './registration'
+import { Context } from './context'
+import { Bundle } from './bundle'
+
+describe('Registration', () => {
+  const target = jest.fn((bundle?: Bundle) => ({
+    timestamp: Date.now(),
+    bundle,
+  }))
+
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
+  describe('calling resolve with singleton lifecycle', () => {
+    it('resolves using the default root context (singleton)', () => {
+      const registration = Registration.create(target)
+      const resolution_0 = registration.resolve()
+      const resolution_1 = registration.resolve()
+      expect(resolution_0).toBe(resolution_1)
+      expect(target).toHaveBeenCalledTimes(1)
+    })
+
+    it('resolves with a provided bundle', () => {
+      const bundle = { foo: 'bar' }
+      const registration = Registration.create(target)
+      const resolution = registration.resolve({ bundle })
+      expect(resolution.bundle).toBe(bundle)
+    })
+  })
+
+  describe('calling resolve with transient lifecycle', () => {
+    it('returns a new resolution every time', () => {
+      const registration = Registration.create(target, { lifecycle: 'transient' })
+      const resolution_0 = registration.resolve()
+      const resolution_1 = registration.resolve()
+      expect(resolution_0).not.toBe(resolution_1)
+      expect(target).toHaveBeenCalledTimes(2)
+    })
+
+    it('passes the bundle to the target', () => {
+      const bundle = { foo: 'bar' }
+      const registration = Registration.create(target, { lifecycle: 'transient' })
+      const resolution = registration.resolve({ bundle })
+      expect(resolution.bundle).toBe(bundle)
+    })
+  })
+
+  describe('calling resolve with scoped lifecycle', () => {
+    it('resolves using the provided context', () => {
+      const context = new Context()
+      const registration = Registration.create(target, { lifecycle: 'scoped' })
+
+      const resolution_0 = registration.resolve({ context })
+      const resolution_1 = registration.resolve({ context })
+      expect(resolution_0).toBe(resolution_1)
+      expect(target).toHaveBeenCalledTimes(1)
+    })
+
+    it('throws if no context is provided for scoped lifecycle', () => {
+      const registration = Registration.create(target, { lifecycle: 'scoped' })
+      expect(() => registration.resolve()).toThrow(/Could not resolve scoped registration\. Missing resolution context/)
+    })
+  })
+
+  describe('calling resolve with invalid lifecycle', () => {
+    it('throws for invalid lifecycle types', () => {
+      const registration = Registration.create(target)
+      
+      // @ts-expect-error
+      registration['lifecycle'] = 'invalid'
+
+      expect(() => registration.resolve()).toThrow(/Invalid lifecycle: invalid/)
+    })
+  })
+
+  describe('clone', () => {
+    it('creates a copy of the registration with merged options', () => {
+      const registration = Registration.create(target, {
+        lifecycle: 'singleton',
+      })
+
+      const bundle = { x: 1 }
+      const context = new Context()
+
+      const registrationClone = registration.clone({ 
+        bundle,
+        context,
+      })
+
+      const resolution_0 = registration.resolve()
+      const resolution_1 = registrationClone.resolve()
+
+      expect(resolution_0).not.toBe(resolution_1)
+      expect(target).toHaveBeenCalledTimes(2)
+    })
+
+    it('inherits the lifecycle from original registration', () => {
+      const registration = Registration.create(target, { lifecycle: 'transient' })
+      const registrationClone = registration.clone()
+      const resolution_0 = registrationClone.resolve()
+      const resolution_1 = registrationClone.resolve()
+      expect(resolution_0).not.toBe(resolution_1)
+    })
+  })
+})
---
+++ src/registration.ts
@@ -0,0 +1,65 @@
+import { Bundle } from './bundle'
+import { Target } from './target'
+import { Context } from  './context'
+
+type RegistrationLifecycle =
+  | 'scoped' // Return an instance from the scope context if one exist
+  | 'transient' // Will always return a new instance of the registration target
+  | 'singleton' // Will return an instance from the root context if one exist
+
+export type RegistrationOptions = {
+  bundle?: Bundle | undefined
+  context?: Context | undefined,
+  lifecycle?: RegistrationLifecycle,
+}
+
+export class Registration<R = any> {
+  public static create<R>(target: Target<R>, options?: RegistrationOptions) {
+    return new Registration<R>(target, options)
+  }
+
+  private readonly target: Target<R>
+
+  private readonly bundle: Bundle
+
+  private readonly context: Context
+
+  private readonly lifecycle: RegistrationLifecycle
+
+  protected constructor(target: Target<R>, options?: RegistrationOptions) {
+    this.target = target
+    this.bundle = options?.bundle ?? {}
+    this.context = options?.context ?? new Context()
+    this.lifecycle = options?.lifecycle ?? 'singleton'
+  }
+
+  public clone(options?: RegistrationOptions): Registration<R> {
+    return new Registration(this.target, {
+      bundle: options?.bundle ?? this.bundle,
+      context: options?.context ?? this.context, 
+      lifecycle: this.lifecycle,
+    })
+  }
+  
+  public resolve(options?: RegistrationOptions): R {
+    const lifecycle = options?.lifecycle ?? this.lifecycle
+
+    if(lifecycle === 'transient') {
+      return this.target(options?.bundle ?? {}) 
+    }
+
+    if(lifecycle === 'singleton') {
+      return this.context.resolve(this.target, options?.bundle)
+    }
+
+    if (options?.context && lifecycle === 'scoped') {
+      return options.context.resolve(this.target, options.bundle)
+    }
+
+    if(!options?.context && lifecycle === 'scoped') {
+      throw new Error('Could not resolve scoped registration. Missing resolution context')
+    }
+
+    throw new Error(`Could not resolve registration. Invalid lifecycle: ${this.lifecycle}.`)
+  }
+}
