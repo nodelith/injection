de9e14836fa6f29af3dd3a3bcaa062892c5687b0
Thomaz Zandonotto <thomaz.zandonotto@gmail.com>
2025-06-25 00:54:31 -0700

    Move resolve function to the end of Module implementation

--- src/module.ts
+++ src/module.ts
@@ -11,48 +11,35 @@ type ModuleRegistrationOptions<R = any> =
 export class Module {
   private readonly container = Container.create<Registration>()
 
+  public register(token: Token, options: ModuleRegistrationOptions): void {
+    const resolver = Resolver.create(options)
+    this.setRegistration(token, resolver, options)
+  }
+
+  public registerFactory(token: Token, factory: TargetFactory, options: RegistrationOptions): void {
+    const resolver = Resolver.create({ factory })
+    this.setRegistration(token, resolver, options)
+  }
+
+  public registerConstructor(token: Token, constructor: TargetConstructor, options: RegistrationOptions): void {
+    const resolver = Resolver.create({ constructor })
+    this.setRegistration(token, resolver, options)
+  }
+
+  private setRegistration<R extends object>(token: Token, resolver: Resolver<R>, options?: RegistrationOptions): void {
+    if (this.container.has(token)) {
+      throw new Error(`Could not register token "${token.toString()}". Module already contains a registration assigned to the same token.`)
+    }
+
+    const registration = Registration.create(resolver, options)
+    this.container.register(token, registration)
+  }
+
   public resolve<T>(token: Token): T {
     if(!this.container.has(token)) {
       throw new Error(`Could not resolve token "${token.toString()}". Module does not contain a registration associted to the given token.`)
     }
 
     return this.container.resolve(token)  
-  }
-
-  public register(token: Token, options: ModuleRegistrationOptions): void {
-    const resolver = Resolver.create(options)
-    this.setRegistration(token, resolver, options)
-  }
-
-  public registerFactory(
-    token: Token,
-    factory: TargetFactory,
-    options: RegistrationOptions,
-  ): void {
-    const resolver = Resolver.create({ factory })
-    this.setRegistration(token, resolver, options)
-  }
-
-  public registerConstructor(
-    token: Token,
-    constructor: TargetConstructor,
-    options: RegistrationOptions,
-  ): void {
-    const resolver = Resolver.create({ constructor })
-    this.setRegistration(token, resolver, options)
-  }
-
-  private setRegistration<R extends object>(
-    token: Token, 
-    resolver: Resolver<R>,
-    options?: RegistrationOptions
-  ): void {
-    if (this.container.has(token)) {
-      throw new Error(`Could not register token "${token.toString()}". Module already contains a registration assigned to the same token.`)
-    }
-
-    const registration = Registration.create(resolver, options)
-
-    this.container.register(token, registration)
   }
 }
