e10732144f0d1f8c926844ca1fbd88fea9f7b251
Thomaz Zandonotto <thomaz.zandonotto@gmail.com>
2025-06-29 11:21:13 -0700

    Declare Wrapper types for Targets

--- src/target.ts
+++ src/target.ts
@@ -1,7 +1,18 @@
-export type Target<R extends object = object, P extends readonly any[] = any[]> = TargetFactory<R, P> | TargetConstructor<R, P>
+export type Target<T extends object = object, P extends readonly any[] = any[]> = TargetFactory<T, P> | TargetConstructor<T, P>
 
-export type TargetFactory<R extends object = object, P extends readonly any[] = any[]> = (...parameters: P) => R
+export type TargetFactory<T extends object = object, P extends readonly any[] = any[]> = (...parameters: P) => T
 
-export type TargetConstructor<R extends object = object, P extends readonly any[] = any[]> = 
-  | ( new (...parameters: P) => R ) 
-  | { new (...parameters: P): R }
+export type TargetConstructor<T extends object = object, P extends readonly any[] = any[]> = 
+  | ( new (...parameters: P) => T ) 
+  | { new (...parameters: P): T }
+
+export type TargetWrapper<T extends object = object, P extends readonly any[] = any[]> = 
+  | TargetConstructorWrapper<T, P>
+  | TargetFactoryWrapper<T, P>
+
+export type TargetFactoryWrapper<T extends object = object, P extends readonly any[] = any[]> = {
+  factory: TargetFactory<T, P>
+}
+export type TargetConstructorWrapper<T extends object = object, P extends readonly any[] = any[]> = {
+  constructor: TargetConstructor<T, P>
+}
