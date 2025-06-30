e4dd1ad0eca912b77b2837b2751bc80d44ea9d4a
Thomaz Zandonotto <thomaz.zandonotto@gmail.com>
2025-06-30 15:59:07 -0700

    Improve Target types declaration

--- src/target.ts
+++ src/target.ts
@@ -1,18 +1,41 @@
-export type Target<T extends object = object, P extends readonly any[] = any[]> = TargetFactory<T, P> | TargetConstructor<T, P>
+export type StaticTarget<T extends any = any> = T
 
-export type TargetFactory<T extends object = object, P extends readonly any[] = any[]> = (...parameters: P) => T
+export type FunctionTarget<T extends any = any, P extends Array<any> = any[]> = (...params: P) => T
 
-export type TargetConstructor<T extends object = object, P extends readonly any[] = any[]> = 
-  | ( new (...parameters: P) => T ) 
-  | { new (...parameters: P): T }
+export type FactoryTarget<T extends object = object, P extends Array<any> = any[]> = (...params: P) => T
 
-export type TargetWrapper<T extends object = object, P extends readonly any[] = any[]> = 
-  | TargetConstructorWrapper<T, P>
-  | TargetFactoryWrapper<T, P>
+export type ConstructorTarget<T extends object = object, P extends Array<any> = Array<any>> = 
+  | ( new (...params: P) => T ) 
+  | { new (...params: P): T }
 
-export type TargetFactoryWrapper<T extends object = object, P extends readonly any[] = any[]> = {
-  factory: TargetFactory<T, P>
+export type ValueTarget<T extends any = any> =
+  | FunctionTarget<T>
+  | StaticTarget<T>
+
+export type ObjectTarget<T extends object = any> =
+  | ConstructorTarget<T>
+  | FactoryTarget<T>
+
+export type ConstructorTargetWrapper<T extends object> = {
+  constructor: ConstructorTarget<T>
 }
-export type TargetConstructorWrapper<T extends object = object, P extends readonly any[] = any[]> = {
-  constructor: TargetConstructor<T, P>
+
+export type FactoryTargetWrapper<T extends object> = {
+  factory: FactoryTarget<T> 
 }
+
+export type FunctionTargetWrapper<T extends any> = {
+  function: FunctionTarget<T>
+}
+
+export type StaticTargetWrapper<T extends any> = {
+  static: StaticTarget<T> 
+}
+
+export type ObjectTargetWrapper<T extends object> = 
+  | ConstructorTargetWrapper<T>
+  | FactoryTargetWrapper<T>
+
+export type ValueTargetWrapper<T extends any> = 
+  | FunctionTargetWrapper<T>
+  | StaticTargetWrapper<T>
